ddc8601297d5a33be1e2884037191779c10c172c
muriarty1983 <murateker9166@gmail.com>
2024-10-09 15:09:50 +0300

    responsive terminal chat

--- main.py
+++ main.py
@@ -1,7 +1,31 @@
 from langchain_ollama import OllamaLLM
+from langchain_core.prompts import ChatPromptTemplate
+
+template="""
+Answer the question below.
+
+Here is the conversation history: {context}
+
+Question: {question}
+
+Answer:
+"""
 
 model = OllamaLLM(model = "llama3.2:latest")
+prompt = ChatPromptTemplate.from_template(template)
+chain = prompt | model
 
-result = model.invoke(input="write a poem in the style of bukowski about hating flies")
-
-print(result)
+def handle_conv():
+    context=""
+    print("Welcome to the conversation! type 'exit' to end the conversation")
+    while True:
+        user_input = input("You: ")
+        if user_input.lower() == "exit":
+            break
+        
+        result = chain.invoke({"context":context,"question":user_input})
+        print("Bot: ",result)
+        context+= f"\nUser: {user_input}\nAI: {result}"
+    
+if __name__ == "__main__":
+    handle_conv()

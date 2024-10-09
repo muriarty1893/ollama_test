30b34cbb524c7833b1cc1d449f1fbd7d488b6b3d
muriarty1983 <murateker9166@gmail.com>
2024-10-09 12:08:02 +0300

    feat: asking wise llama for coding enlightenment (hope it speaks python 🐍)

--- test.py
+++ test.py
@@ -1,11 +1,18 @@
 import ollama
 
 desiredModel='llama3.2:latest'
-questionToAsk='What is the capital of France?'
+questionToAsk='What is thebest strat to learn coding ?'
 
 response = ollama.chat(model=desiredModel, messages=[
     {
-        'role': 'system',
+        'role': 'user',
         'content': questionToAsk,
     },
 ])
+
+OllamaResponse = response['message']['content']
+
+print(OllamaResponse)
+
+with open('OutputOllama.txt', 'w', encoding="utf-8") as text_file:
+    text_file.write(OllamaResponse)

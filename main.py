d97ff7a4bfabc65b18094e22511ae356216828b0
muriarty1983 <murateker9166@gmail.com>
2024-10-10 15:11:06 +0300

    clean up

--- main.py
+++ main.py
@@ -3,9 +3,6 @@ from langchain_core.prompts import ChatPromptTemplate
 from datetime import datetime  # Tarih ve saat eklemek için
 
 template = """
-Act as a Muslim imam who gives me guidance and advice on how to deal with life problems. 
-Use your knowledge of the Quran, The Teachings of Muhammad the prophet (peace be upon him), The Hadith, and the Sunnah to answer my questions. 
-Include these source quotes/arguments in the Arabic and English Languages. 
 Answer the question below.
 
 Here is the conversation history: {context}

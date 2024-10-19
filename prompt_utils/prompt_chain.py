4673a4de6aacaf46851ce3a66ccdd1a908087db2
muriarty1983 <murateker9166@gmail.com>
2024-10-19 13:47:09 +0300

    wrong repo push fix

--- prompt_utils/prompt_chain.py
+++ prompt_utils/prompt_chain.py
@@ -3,13 +3,13 @@ from langchain_core.prompts import ChatPromptTemplate
 
 def create_prompt_chain():
     template = """
-    You are an expert job consultant AI. Based on the following document content, provide job recommendations, insights, or potential career paths that could be suitable.
+    You are an expert at C++ and you are going to write a windows desktop app in C++ language. I will gice you pdf of a book that my teacher wrote. Its about ICBYTES library in C++ that my teacher wrote. Here's what is the app about;do a animation that at least 3 seconds, there will be multiple objects that  move in different speed and at the same time. Also therell be a start button for me to start the animation.
 
-    Here is the content of the document:
+    Here is the content of the document for you to learn ICBYTES library:
 
     {pdf_content}
 
-    Give your recommendations:
+    Now write the code:
     """
     desiredModel = OllamaLLM(model="llama3.2:latest")
     prompt = ChatPromptTemplate.from_template(template)
